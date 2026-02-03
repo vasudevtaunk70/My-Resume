b6f7cd019000eec7ab63d4ddbf31cacd92c55eda
vasudevtaunk70 <vasudevtaunk70@gmail.com>
2026-02-03 14:47:11 +0530

    Update Projects.jsx

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -23,21 +23,14 @@ export default function Projects() {
           {/* Content */}
           <div>
             <h3 className="text-2xl font-semibold mb-3">
-              DataMorph
+              SEO Developement
             </h3>
             <p className="text-gray-600 mb-2">
               A package that allows you to convert your data between different formats.
             </p>
             <p className="text-gray-600 mb-5">
               Supports YAML, JSON, CSV and INI with multiple conversion options.
-            </p>
-
-            <a
-              href="#"
-              className="inline-block px-6 py-3 bg-black text-white rounded-lg hover:bg-gray-800 transition"
-            >
-              View Project
-            </a>
+            </p>            
           </div>
         </div>
 
@@ -56,34 +49,66 @@ export default function Projects() {
           {/* Content */}
           <div className="md:order-1">
             <h3 className="text-2xl font-semibold mb-3">
-              Sketch Tab – Your Art Playground
+              Shopify Speed Optimization
             </h3>
             <p className="text-gray-600 mb-2">
               Offline drawing app with a clean and user-friendly interface.
             </p>
             <p className="text-gray-600 mb-5">
               Play with creative tools and enjoy a smooth drawing experience.
-            </p>
-
-            <a
-              href="#"
-              className="inline-block px-6 py-3 bg-black text-white rounded-lg hover:bg-gray-800 transition"
-            >
-              View Project
-            </a>
+            </p>            
           </div>
         </div>
+        {/* Project 3 */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center mb-16">
 
-        {/* More Projects Button */}
-        <div className="text-center">
-          <a
-            href="#"
-            className="inline-block px-8 py-3 border border-black rounded-lg hover:bg-black hover:text-white transition"
-          >
-            More Projects
-          </a>
+          {/* Image */}
+          <div className="flex justify-center">
+            <img
+              src="https://github.com/smv1999/datamorph/blob/main/assets/datamorph.jpg?raw=true"
+              alt="DataMorph"
+              className="w-40 md:w-48 rounded-xl shadow-lg"
+            />
+          </div>
+
+          {/* Content */}
+          <div>
+            <h3 className="text-2xl font-semibold mb-3">
+              Shopify Developement
+            </h3>
+            <p className="text-gray-600 mb-2">
+              A package that allows you to convert your data between different formats.
+            </p>
+            <p className="text-gray-600 mb-5">
+              Supports YAML, JSON, CSV and INI with multiple conversion options.
+            </p>            
+          </div>
+        </div>        
+        {/* Project 4 */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center mb-16">
+
+          {/* Image */}
+          <div className="flex justify-center md:order-2">
+            <img
+              src="https://play-lh.googleusercontent.com/arjwweu2ecu2V3OWozZUYYm67xWir-8njlyBtmayu33nhmZTFuBZ_E5ZkJe2-loNhg=w240-h480-rw"
+              alt="Sketch Tab"
+              className="w-40 md:w-48 rounded-xl shadow-lg"
+            />
+          </div>
+
+          {/* Content */}
+          <div className="md:order-1">
+            <h3 className="text-2xl font-semibold mb-3">
+              Wordpress Speed Optimization
+            </h3>
+            <p className="text-gray-600 mb-2">
+              Offline drawing app with a clean and user-friendly interface.
+            </p>
+            <p className="text-gray-600 mb-5">
+              Play with creative tools and enjoy a smooth drawing experience.
+            </p>            
+          </div>
         </div>
-
       </div>
     </section>
   )
