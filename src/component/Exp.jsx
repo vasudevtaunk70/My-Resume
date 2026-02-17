605e6dafb5fa584df7d41ee10a045bb222467afa
vasudevtaunk70 <vasudevtaunk70@gmail.com>
2026-02-17 17:12:06 +0530

    Update Exp.jsx

--- src/component/Exp.jsx
+++ src/component/Exp.jsx
@@ -4,7 +4,8 @@ export default function Exp() {
             <div className="max-w-6xl mx-auto px-6 py-20">                
                 <h2 className="text-4xl font-bold text-center text-white mb-14">
                     Experience
-                </h2>                
+                </h2>   
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 <div className="space-y-8">  
                     <div className="bg-[#27292d] rounded-xl p-6 shadow-lg">
                         <h3 className="text-2xl font-semibold text-white">
@@ -91,6 +92,7 @@ export default function Exp() {
                             </div>                        
                         </div>
                     </div>                                            
+                </div>
                 </div>
             </div>
         </section>
